753d714d962640923f2118f9bb30a4016dcc6245
Suraj <suraj.krishnaswami@gmail.com>
2015-10-04 19:21:36 -0400

    Detection to find base64 content.

--- cryptana/cryptana1.0/source/cryptana/algorithms/base64.cpp
+++ cryptana/cryptana1.0/source/cryptana/algorithms/base64.cpp
@@ -22,6 +22,7 @@
 
 #include <errno.h>
 #include "base64.h"
+#include "../debug.h"
 
 
 ////////////////////////////////////////////////////////////
@@ -48,9 +49,68 @@ int
 CBase64::ApplyAlgorithm(
 	const unsigned char *pcuchBuffer,
 	unsigned int uicchBuffer,
-	void *vpReserved
+	BASE64 *pBase64
 	)
 {
+	int iVal;
+	unsigned int uiLoop;
 
+	INIT_LOGGER(CBase64::ApplyAlgorithm)
+
+	if (0 == pcuchBuffer || 0 == pBase64)
+	{
+		LOG("Parameter validation failed. pcuchBuffer or pBase64 is NULL. Exit");
+		return EINVAL;
+	}
+
+	//
+	//	Check if size is multiple of 4.
+	//
+	if (0 != (uicchBuffer%4))
+	{
+		LOG("The input buffer is not in size of 4. Hence not base 64 content");
+		LOG("Size = %d", uicchBuffer);
+
+		pBase64->bIsBase64 = false;
+		return 0;
+	}
+
+	LOG("Data is in multiple of 4. Checking further conditions.");
+
+	for (uiLoop = 0; uiLoop < uicchBuffer; uiLoop++)
+	{
+		iVal = pcuchBuffer[uicchBuffer];
+
+		if (iVal >= 'A' && iVal <= 'Z')
+		{
+			continue;
+		}
+
+		if (iVal >= 'a' && iVal <= 'z')
+		{
+			continue;
+		}
+
+		if (iVal == '+' || iVal == '/')
+		{
+			continue;
+		}
+
+		if (iVal == '=')
+		{
+			LOG("character is %c", iVal);
+			LOG("Position: %d", (uicchBuffer - uiLoop));
+			if (2 == uicchBuffer - uiLoop || 1 == uicchBuffer - uiLoop)
+			{
+				continue;
+			}
+		}
+
+		pBase64->bIsBase64 = false;
+		return 0;
+	}
+
+	pBase64->bIsBase64 = true;
 	return 0;
 }
+
